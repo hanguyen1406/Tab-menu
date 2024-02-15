c02f53cb46d6894219b29ab9c6f92a204a6def1d
hanhat1406 <hgnguyen47347714@gmail.com>
2024-02-15 10:31:46 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+$("#leftside-navigation .sub-menu > a").click(function(e) {
+  $("#leftside-navigation ul ul").slideUp(), $(this).next().is(":visible") || $(this).next().slideDown(),
+  e.stopPropagation()
+})
+
+const testLog = (i) => {
+  console.log(i);
+}
