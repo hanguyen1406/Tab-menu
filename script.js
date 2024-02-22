263293940d67ff4bdefe2be08b67f0e845daec86
hanguyen1406 <hgnguyen47347714@gmail.com>
2024-02-22 08:05:20 +0700

    xong phan doi cho

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 var noi = $(".sub-menu").length;
 var index = 0;
 
-const addEventForItem = () => {
+function removeAllEventListeners(element) {
+    var newElement = element.cloneNode(true);
+    element.outerHTML = newElement.outerHTML;
+    return newElement;
+}
+
+const addEventForItem = async () => {
+    $("#leftside-navigation .sub-menu > a").off("click");
     $("#leftside-navigation .sub-menu > a").on("click", function (e) {
         index = $("#leftside-navigation .sub-menu").index($(this).parent());
         $("#leftside-navigation ul ul").slideUp(),
@@ -9,70 +16,76 @@ const addEventForItem = () => {
         e.stopPropagation();
         // console.log(index);
     });
-};
-$("#leftside-navigation .sub-menu ul li .up2").on("click", function(e) {
-    console.log(this);
-    e.preventDefault(); // Prevent the default behavior of the anchor element
-    var listItem = $(this).parent().parent(); // Get the parent <li> element
-    var prevListItem = listItem.prev(); // Get the previous <li> element
+    addEventForItem2();
 
-    // Check if there's a previous <li> element
-    if (prevListItem.length) {
-        // Swap the <li> elements
-        listItem.insertBefore(prevListItem);
-    }
-});
-$("#addItem").click(() => {
-    var userInput = window.prompt("Điền tên tiêu đề:");
-    // Check if the user entered something
-    if (userInput !== null) {
-        console.log(userInput);
-    }
-});
-
-for (let i = 0; i < noi; i++) {
-    var imageUploadInput = document.getElementById("image" + (i + 1));
-    imageUploadInput.addEventListener("change", function (event) {
-        var selectedFile = event.target.files[0];
-        if (selectedFile) {
-            var formData = new FormData();
-            formData.append("imageFile", selectedFile);
-
-            // Perform AJAX request to upload file
-            var xhr = new XMLHttpRequest();
-            xhr.open("POST", "saveIcon.php", true);
-            xhr.onload = function () {
-                if (xhr.status === 200) {
-                    console.log(xhr.responseText);
-                } else {
+    for (let i = 0; i < noi; i++) {
+        var imageUploadInput = document.querySelectorAll(".image")[i];
+        console.log(imageUploadInput);
+        // await removeAllEventListeners(imageUploadInput);
+        imageUploadInput.addEventListener("change", function (event) {
+            var selectedFile = event.target.files[0];
+            if (selectedFile) {
+                var formData = new FormData();
+                formData.append("imageFile", selectedFile);
+                index = i;
+                console.log("index: " + index);
+                // Perform AJAX request to upload file
+                var xhr = new XMLHttpRequest();
+                xhr.open("POST", "saveIcon.php", true);
+                xhr.onload = function () {
+                    if (xhr.status === 200) {
+                        // console.log(xhr.responseText);
+                        $(".iconTitle")
+                            .eq(index)
+                            .attr("src", `./img/${selectedFile.name}`);
+                    } else {
+                        console.log("Error uploading image!");
+                    }
+                };
+                xhr.onerror = function () {
                     console.log("Error uploading image!");
-                }
-            };
-            xhr.onerror = function () {
-                console.log("Error uploading image!");
-            };
-            xhr.send(formData);
-        } else {
-            console.log("No file selected");
+                };
+                xhr.send(formData);
+            } else {
+                console.log("No file selected");
+            }
+        });
+    }
+};
+function addEventForItem2() {
+    $("#leftside-navigation .sub-menu ul li .up2").on("click", function (e) {
+        console.log(this);
+        e.preventDefault();
+        var listItem = $(this).parent().parent();
+        var prevListItem = listItem.prev();
+
+        if (prevListItem.length) {
+            listItem.insertBefore(prevListItem);
         }
     });
+    $("#leftside-navigation .sub-menu ul li .down2").on("click", function (e) {
+        console.log(this);
+        e.preventDefault();
+        var listItem = $(this).parent().parent();
+        var nextListItem = listItem.next();
+
+        if (nextListItem.length) {
+            nextListItem.insertBefore(listItem);
+        }
+    });
+
+    $("#leftside-navigation .sub-menu ul li .delete2").on(
+        "click",
+        function (e) {
+            var result = confirm("Chắc chắn xóa?");
+            if (result) {
+                $(this).closest("li").remove();
+            }
+        }
+    );
 }
+
 // Add an event listener for when a file is selected
-
-const upItem = (i) => {
-    console.log("up:", i);
-    if (i > 0) {
-        var a = $(".sub-menu").eq(index).children("ul");
-        console.log(a.html());
-    }
-};
-
-const downItem = (i) => {
-    console.log("down:", i);
-};
-const deleteItem = (i) => {
-    console.log("delete:", i);
-};
 
 const testLog = (i) => {
     console.log(i);
@@ -99,7 +112,7 @@ $(document).ready(() => {
         }
     });
 
-    $("#up").click(() => {
+    $("#up").click(async () => {
         if (index > 0) {
             var html1 = $(".sub-menu").eq(index).html();
             var html2 = $(".sub-menu")
@@ -109,16 +122,15 @@ $(document).ready(() => {
                 `Chắc chắn đổi chỗ tiêu đề ${index - 1} và ${index}`
             );
             if (result) {
-                $(".sub-menu")
+                await $(".sub-menu")
                     .eq(index - 1)
                     .html(html1);
-                $(".sub-menu").eq(index).html(html2);
-                $("#leftside-navigation .sub-menu > a").off("click");
+                await $(".sub-menu").eq(index).html(html2);
                 addEventForItem();
             }
         }
     });
-    $("#down").click(() => {
+    $("#down").click(async () => {
         if (index < noi - 1) {
             var html1 = $(".sub-menu").eq(index).html();
             var html2 = $(".sub-menu")
@@ -128,11 +140,10 @@ $(document).ready(() => {
                 `Chắc chắn đổi chỗ tiêu đề ${index} và ${index + 1}`
             );
             if (result) {
-                $(".sub-menu").eq(index).html(html2);
-                $(".sub-menu")
+                await $(".sub-menu").eq(index).html(html2);
+                await $(".sub-menu")
                     .eq(index + 1)
                     .html(html1);
-                $("#leftside-navigation .sub-menu > a").off("click");
                 addEventForItem();
             }
         }
@@ -144,40 +155,61 @@ $(document).ready(() => {
         }
     });
     $("#add").click(() => {
-        // var currentItem = $(".sub-menu").eq(index);
-        // var clonedItem = currentItem.clone();
-        // clonedItem
-        //     .children("a")
-        //     .children("label")
-        //     .attr("for", "image" + ++noi);
-        // console.log(clonedItem.html());
-        // currentItem.after(clonedItem);
-        // $("#leftside-navigation .sub-menu > a").off("click");
-        // addEventForItem();
         var itemName = prompt(`Tên tiêu đề thêm mới:`);
         if (itemName) {
             $(".sub-menu").eq(index).children("ul").append(`
             <li>
                 <a>${itemName}</a>
                 <span style="margin-top: 10px">
-                    <img class="titleIcon" src="./img/up.png" />
+                    <img class="titleIcon up2" src="./img/up.png" />
                     <img
-                        class="titleIcon"
+                        class="titleIcon down2"
                         src="./img/down.png"
                     />
                     <img
-                        class="titleIcon"
+                        class="titleIcon delete2"
                         src="./img/delete.png"
                     />
                 </span>
             </li>`);
+            addEventForItem();
         }
     });
     $("#rename").click(() => {
-        var userInput = prompt(`Tên tiêu đề ${index} mới:`);
+        var userInput = prompt(
+            `Tên tiêu đề ${index} mới:`,
+            $(".title").eq(index).html()
+        );
 
         if (userInput !== null) {
             $(".title").eq(index).html(userInput);
+        }
+    });
+    $("#addItem").click(() => {
+        var userInput = prompt(`Tên tiêu đề thêm mới:`);
+        noi++;
+        if (userInput !== null) {
+            $(".nano-content").append(`
+            <li class="sub-menu">
+                <a>
+                    <label for="image${noi}">
+                        <img
+                            class="titleIcon iconTitle"
+                            src="./img/list.png"
+                        />
+                        <input
+                            type="file"
+                            id="image${noi}"
+                            accept="image/*"
+                        />
+                    </label>
+                    <span class="title">${userInput}</span>
+                    <i class="arrow fa fa-angle-right pull-right"></i>
+                </a>
+                <ul></ul>
+            </li>
+            `);
+            addEventForItem();
         }
     });
 });
